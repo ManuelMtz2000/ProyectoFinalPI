2006199a29c5fb419513385030fe0a96da5b7eaa
ManuelMtz2000 <manuelmtzt025@gmail.com>
2020-11-02 20:46:49 -0600

    Terminado inventario

--- resources/views/inventario/inventarioShow.blade.php
+++ resources/views/inventario/inventarioShow.blade.php
@@ -1,31 +1,35 @@
 @extends('layouts.app')
 @section('contenido')
-    <h1>Información del producto.</h1>
-    <a href="{{ route('inventario.edit', [$inventario->id]) }}">Editar información del producto.</a>
-    <br>
-    <table>
-        <tr>
-            <th>ID</th>
-            <th>Nombre</th>
-            <th>Categoria</th>
-            <th>Precio</th>
-            <th>Precio para clientes</th>
-            <th>Cantidad disponible</th>
-            <th>Descripción</th>
-        </tr>
+    <h1 class="display-3">Información del producto.</h1>
+    <hr class="sidebar-divider">
+    <a class="btn btn-primary" role="button" href="{{ route('inventario.edit', [$inventario->id]) }}">Editar información del producto.</a>
+    <a class="btn btn-primary" role="button" href="{{ route('inventario.index') }}">Regresar a la lista</a>
+    <br><br>
+    <div class="table-responsive">
+        <table class="table table-striped">
             <tr>
-                <td>{{ $inventario->id }}</td>
-                <td>{{ $inventario->nombre }}</td>
-                <td>{{ $inventario->categoria }}</td>
-                <td>{{ $inventario->precio}}</td>
-                <td>{{ $inventario->precio_cliente }}</td>
-                <td>{{ $inventario->cantidad }}</td>
-                <td>{{ $inventario->descripcion }}</td>
+                <th>ID</th>
+                <th>Nombre</th>
+                <th>Categoria</th>
+                <th>Precio</th>
+                <th>Precio para clientes</th>
+                <th>Cantidad disponible</th>
+                <th>Descripción</th>
             </tr>
-    </table>
-    <form action="{{ route('inventario.destroy',[$inventario]) }}" method="POST">
-        @csrf
-        @method('DELETE')
-        <button type="submit">Eliminar</button>
-    </form>
+                <tr>
+                    <td>{{ $inventario->id }}</td>
+                    <td>{{ $inventario->nombre }}</td>
+                    <td>{{ $inventario->categoria }}</td>
+                    <td>{{ $inventario->precio}}</td>
+                    <td>{{ $inventario->precio_cliente }}</td>
+                    <td>{{ $inventario->cantidad }}</td>
+                    <td>{{ $inventario->descripcion }}</td>
+                </tr>
+        </table>
+        <form action="{{ route('inventario.destroy',[$inventario]) }}" method="POST">
+            @csrf
+            @method('DELETE')
+            <button type="submit" class="btn btn-primary">Eliminar producto</button>
+        </form>
+    </div>
 @endsection
