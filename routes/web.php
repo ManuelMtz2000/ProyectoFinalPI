8fc1296cc93adceaa3aca01dab5ebc32a9aa7a8e
ManuelMtz2000 <manuelmtzt025@gmail.com>
2020-11-05 20:27:58 -0600

    Pequeños cambios

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/inicio', function() {
     return view('inicio');
 });
+/*Route::get('/inicio', function() {
+    return view('inicio');
+});*/
 Route::resource('inventario', InventarioController::class);
